e8c70798a866cee4c375fe6e952d793d7a1d4f60
Niranjan K D <niranjandeshmukh371@gmail.com>
2023-07-19 02:40:01 +0530

    Initial commit

---
+++ RecognitionProject.py
@@ -0,0 +1,106 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+import tkinter
+
+from time import sleep
+from datetime import datetime
+from tkinter import messagebox
+from tkinter import *
+from tkinter import ttk
+
+path = 'ImagesRecord'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def markAccess(name):
+    with open('AccessRecord.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+
+def startCap():
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        success, img = cap.read()
+        #img = captureScreen()
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            #print(faceDis)
+            matchIndex = np.argmin(faceDis)
+
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            else:
+                name = "Unknown"
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 0, 255), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+                sleep(2)
+                root = tkinter.Tk()
+                root.withdraw()
+                messagebox.showwarning("Warning", "Warning: Access Denied ! \n You are not allowed to access the system")
+
+            markAccess(name)
+
+        cv2.imshow('Webcam', img)
+        cv2.waitKey(1)
+
+
+
+win = Tk()
+
+# Set the size of the tkinter window
+win.geometry("700x350")
+win.wm_title("FaceLock App")
+# Add an optional Label widget
+Label(win, text= "Welcome !", font= ('Aerial 17 bold italic')).pack(pady= 30)
+
+# Create a Button to display the message
+ttk.Button(win, text= "Click Here to verify your Login", command=startCap).pack(pady= 20)
+win.mainloop()
